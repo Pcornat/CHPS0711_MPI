efa9a0ced9fe5c82d3d95edbd6d8b2bfdb537e30
Florent Denef <florent.denef@gmail.com>
2018-11-20 11:49:11 +0100

    In progress to adapt this program for MPI.

--- Eratosthene.c
+++ Eratosthene.c
@@ -15,38 +15,42 @@ int main(int argc, char* argv[]) {
 	int taille;            /* Taille du tableau de nombres premiers a verifier */
 	int* marques;        /* Tableau permettant de marquer les nombres verifies */
 	int indexMarques;    /* Index du nb premier courant dans le tableau "marques" */
-	int premierCourant;/* Nombre premier courant */
+	int k;/* Nombre premier courant */
 	int compte;            /* Compteur de nombres premiers */
-	int indDebut;        /* Indicateur de debut de marquage*/
+	int indMin;        /* Indicateur de debut de marquage*/
 	int i;
-	int nbProc, id;
+	int p, rank;
+	int valMin, valMax;
 
 	MPI_Init(&argc, &argv);
 	if (argc != 2) {  /* Verifie que un seul argument a ete fourni */
 		printf("Usage : %s n\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	MPI_Comm_size(MPI_COMM_WORLD, &nbProc);
-	MPI_Comm_rank(MPI_COMM_WORLD, &id);
+	MPI_Comm_size(MPI_COMM_WORLD, &p);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	n = atoi(argv[1]);
 
-	taille = (n - 1)/nbProc;
+	valMin = (rank * (n - 1) / p) + 2, valMax = ((rank + 1) + ((n - 1) / p) + 2) - 1;
+
+	taille = valMax - valMin + 1;
 
 	marques = (int*) malloc(sizeof(int) * taille);
+//#pragma omp parallel for //Pourrait être marrant.
 	for (i = 0; i < taille; i++)
 		marques[i] = 0;
 
 	indexMarques = 0;
-	premierCourant = 2;
+	k = 2;
 	do {
-		indDebut = premierCourant * premierCourant - 2;    /* Indice du 1er nombre a marquer (k2)*/
-		for (i = indDebut; i < taille; i += premierCourant) /* Pour tous les multiples du nb premier, */
+		indMin = rank * (n - 1) / p;    /* Indice du 1er nombre a marquer (k2)*/
+		for (i = indMin; i < taille; i += k) /* Pour tous les multiples du nb premier, */
 			marques[i] = 1;                                            /* on marque le nombre*/
 
 		while (marques[++indexMarques]);        /* On trouve l'indice de la prochaine case non marquee */
-		premierCourant = indexMarques + 2;  /* ce qui donne le nb premier du prochain tour */
+		k = indexMarques + 2;  /* ce qui donne le nb premier du prochain tour */
 
-	} while (premierCourant * premierCourant <= n);  /* On arrete si son carre est > que n*/
+	} while (k * k <= n);  /* On arrete si son carre est > que n*/
 
 	compte = 0;
 	for (i = 0; i < taille; i++)
@@ -61,5 +65,6 @@ int main(int argc, char* argv[]) {
 	printf("\n");
 
 	free(marques);
+	MPI_Finalize();
 	return EXIT_SUCCESS;
 }
